009948fc0c72ca77d2168b2ce2cc5fa044b1016a
Yegor Osipov <83457266+DeadNord@users.noreply.github.com>
2024-01-24 19:49:01 +0100

    fix return

--- search/string_search.py
+++ search/string_search.py
@@ -1,10 +1,9 @@
 def string_search(text, pattern):
-    iterations = 0
     index = text.find(pattern)
-    iterations += 1
+    iterations = 1
 
     while index != -1:
         index = text.find(pattern, index + 1)
         iterations += 1
 
-    return iterations
+    return iterations, index if iterations > 1 else None
